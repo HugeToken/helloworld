4d557b0b1014571f79da4153c16ec4ec2d7bf2fa
Your Name <you@example.com>
2023-03-22 16:12:45 +0900

    asdf

--- 파이썬/0322.py
+++ 파이썬/0322.py
@@ -1,10 +1,1 @@
-
-# Please enter the commit message for your changes. Lines starting
-# with '#' will be ignored, and an empty message aborts the commit.
-#
-# On branch main
-# Your branch is up to date with 'origin/main'.
-#
-# Changes to be committed:
-#	new file:   "\355\214\214\354\235\264\354\215\254/0322.py"
-#
+#2023-03-22
